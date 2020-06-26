0dd57031a8daddc12a0893c30477b625850abc35
Joseph Martin <creative.joe007@gmail.com>
2020-06-27 00:26:41 +0100

    Worked on adjusting the start page from 1 to 0 as google seek number starts from 0

--- main.py
+++ main.py
@@ -10,17 +10,24 @@ arguments = sys.argv
 
 print("\n")
 def main():
-  chrome_options = Options()
-  chrome_options.add_argument('--headless')
-  chrome_options.add_argument('--start-maximized')
+  chrome_options = Options();
+  chrome_options.add_argument('--headless');
+  chrome_options.add_argument('--start-maximized');
+  chrome_options.add_argument("enable-automation");
+  chrome_options.add_argument("--disable-extensions");
+  chrome_options.add_argument("--dns-prefetch-disable");
+  chrome_options.add_argument("--no-sandbox");
+  chrome_options.add_argument("--disable-dev-shm-usage");
+  chrome_options.add_argument("--disable-gpu");
+  chrome_options.add_argument("--force-device-scale-factor=1");
 
   executor_url = ""
   session_id = ""
   query = arguments[1]
-  start_page = int(arguments[2]) if len(arguments) > 2 else 1
-  stop_page = int(arguments[3]) if len(arguments) > 3 else 15
+  start_page = int(arguments[2]) if len(arguments) > 2 and type(arguments[2]) is int else 0
+  stop_page = int(arguments[3]) if len(arguments) > 3 and type(arguments[3]) is int else 14
 
-  if start_page == 0: start_page = 1 # If the user puts in 0, we auto make it one
+  if start_page < 0: start_page = 0 # If the user puts in 0, we auto make it one
   elif (stop_page - start_page) > 15:
     print("You cannot search more than 15 pages at a time")
   # elif (stop_page - start_page) > 15:
