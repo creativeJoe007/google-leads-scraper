b0487a0c70a687560bd3c9fd6d07f7128c28f016
Joseph Martin <creative.joe007@gmail.com>
2020-07-01 00:05:32 +0100

    Worked on styling command-line print statements

---
+++ println.py
@@ -0,0 +1,18 @@
+HEADER = '\033[95m'
+OKBLUE = '\033[94m'
+OKGREEN = '\033[92m'
+WARNING = '\033[93m'
+FAIL = '\033[91m'
+ENDC = '\033[0m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+
+def println(text, _type='fail'):
+ if _type == 'fail':
+   print(f"\n{FAIL} {text} {ENDC}\n")
+ elif _type == 'success':
+   print(f"\n{OKGREEN} {text}  {ENDC}\n")
+ elif _type == 'normal':
+   print(f"\n{OKBLUE} {text}  {ENDC}\n")
+ elif _type == 'warn':
+   print(f"\n{WARNING} {text}  {ENDC}\n")
