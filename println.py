5b7dac5cbead995159f3d09afa287a87bd107dc1
Joseph Martin <creative.joe007@gmail.com>
2020-07-03 01:23:10 +0100

    Worked on print formating

--- println.py
+++ println.py
@@ -1,3 +1,5 @@
+import os
+
 HEADER = '\033[95m'
 OKBLUE = '\033[94m'
 OKGREEN = '\033[92m'
@@ -8,11 +10,14 @@ BOLD = '\033[1m'
 UNDERLINE = '\033[4m'
 
 def println(text, _type='fail'):
- if _type == 'fail':
-   print(f"\n{FAIL} {text} {ENDC}\n")
- elif _type == 'success':
-   print(f"\n{OKGREEN} {text}  {ENDC}\n")
- elif _type == 'normal':
-   print(f"\n{OKBLUE} {text}  {ENDC}\n")
- elif _type == 'warn':
-   print(f"\n{WARNING} {text}  {ENDC}\n")
+  # os.system('color')
+  if _type == 'fail':
+    print(f"\n{FAIL} {text} {ENDC}\n")
+  elif _type == 'success':
+    print(f"\n{OKGREEN} {text}  {ENDC}\n")
+  elif _type == 'normal':
+    print(f"\n{OKBLUE} {text}  {ENDC}\n")
+  elif _type == 'bold':
+    print(f"\n{BOLD} {text}  {ENDC}\n")
+  elif _type == 'warn':
+    print(f"\n{WARNING} {text}  {ENDC}\n")
